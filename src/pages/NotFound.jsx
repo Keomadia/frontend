c588149ac5c70b6f1a61bf64765e2f89436b387f
keomadi <keomadianyankah@example.com>
2025-02-23 20:21:27 +0100

    Commit w

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div style={{ textAlign: "center", padding: "50px" , fontSize: "50px" ,color : "#2b2b2b"}}>
-      <h1>404 - Page Not Found</h1>
+      <h1 style={{ color : "#02274f"}}>404 - Page Not Found</h1>
       <p style={{ fontSize: "30px" , padding: "10px" , margin: "10px" }} >The page you are looking for does not exist.</p>
-      <Link to="/" style={{ fontSize: "20px" , padding: "20px" , margin: "0px" , background: "#2b2b2b" , color : "#fff"}}>Back to Home</Link>
+      <Link to="/" style={{ fontSize: "20px" , padding: "20px" , margin: "0px" , background: "#02274f" , color : "#fff"}}>Back to Home</Link>
     </div>
   );
 };
